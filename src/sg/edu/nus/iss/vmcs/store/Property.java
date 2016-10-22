b14775a80861be775f1111802bc8849efbd52296
koushikcr <c.r.koushik@gmail.com>
2016-10-22 14:04:30 +0800

    Bridge Pattern Initial commit

---
+++ src/sg/edu/nus/iss/vmcs/store/Property.java
@@ -0,0 +1,11 @@
+package sg.edu.nus.iss.vmcs.store;
+
+public   class Property {
+	PropertyLoader propertyLoader;
+	public  Property(PropertyLoader propertyLoader){
+		this.propertyLoader = propertyLoader;
+	}
+	public PropertyLoader getLoader(){
+			return propertyLoader;
+	}
+}
